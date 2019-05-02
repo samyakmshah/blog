6a119baf09238f696a1c89fef0e2062ac5ac3669
Samyak Shah <samyakshah@biggie-2.local>
2019-05-02 17:18:25 -0400

    Website skeleton completed

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Samyak Shah</title>
+  </head>
+  <body>
+
+    <!--Default Bootstrap Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Samyak Shah</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/contact">Contact</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              My Account
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <div class = "container">
+      <div class="jumbotron">
+        <h1 class="display-4">Welcome!</h1>
+        <p class="lead">Welcome to my machine learning, data science and engineering blog. Thank you for visiting.</p>
+        <hr class="my-4">
+        <p>Learn something new</p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Random Article</a>
+        </p>
+      </div> 
+
+      <div class = "row">
+        <div class="col-md-8">
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>sodijf asdfo aiof osdj oaosjfoaj sod joasdi fjaso jfao sdif ajosdif oais jfoasi dfjoaisj ofjaso ifjao sijf oasij foiasj ofia sjodifj aosij foas jodfi aosj oaisdf jaos dfj. 
+            </p>
+            <a href="#" class = "btn btn-primary">Read More</a>
+          </div>
+
+          <hr>
+
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>sodijf asdfo aiof osdj oaosjfoaj sod joasdi fjaso jfao sdif ajosdif oais jfoasi dfjoaisj ofjaso ifjao sijf oasij foiasj ofia sjodifj aosij foas jodfi aosj oaisdf jaos dfj. 
+            </p>
+            <a href="#" class = "btn btn-primary">Read More</a>
+          </div>
+
+          <hr>
+
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>sodijf asdfo aiof osdj oaosjfoaj sod joasdi fjaso jfao sdif ajosdif oais jfoasi dfjoaisj ofjaso ifjao sijf oasij foiasj ofia sjodifj aosij foas jodfi aosj oaisdf jaos dfj. 
+            </p>
+            <a href="#" class = "btn btn-primary">Read More</a>
+          </div>
+
+          <hr>
+
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>sodijf asdfo aiof osdj oaosjfoaj sod joasdi fjaso jfao sdif ajosdif oais jfoasi dfjoaisj ofjaso ifjao sijf oasij foiasj ofia sjodifj aosij foas jodfi aosj oaisdf jaos dfj. 
+            </p>
+            <a href="#" class = "btn btn-primary">Read More</a>
+          </div>
+        </div>
+        <div class = "col-md-3 offset-md-1">
+            <h2>Sidebar</h2>
+        </div>
+    </div> <!-- end of .container -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
